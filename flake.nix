a459676ab4bfdd922e558993d07dc804991c4ee8
ihsan <ihsanl@pm.me>
2021-01-31 15:19:15 +0300

    add source and gitignore and linux support

--- flake.nix
+++ flake.nix
@@ -1,9 +1,17 @@
 {
-	description = "hello world, c";
+	description = "Hello world, in c";
 
-	inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
 	outputs = { self, nixpkgs }: {
+		defaultPackage.x86_64-linux =
+			with import nixpkgs { system = "x86_64-linux"; };
+			stdenv.mkDerivation {
+				name = "hello";
+				src = self;
+				buildPhase = "cc -o hello ./hello.c";
+				installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+			};
 		defaultPackage.x86_64-darwin =
 			with import nixpkgs { system = "x86_64-darwin"; };
 			stdenv.mkDerivation {
--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main(int argc, char** argv) {
 
 	printf("Hello nix flakes.\n");
-	printf(" This line printed from a C code and builded with nix flakes.\n");
+	printf("This line printed from a C code and builded with nix flakes.\n");
 
 	return 0;
 
